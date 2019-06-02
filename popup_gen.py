3956db5a7d3f199f0b36203a39ca0c054cd417ae
jyotipro10 <43803219+jyotipro10@users.noreply.github.com>
2019-06-02 17:47:23 +0530

    Add files via upload

---
+++ popup_gen.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+for i in range(0,10) :
+	webbrowser.open("https://www.youtube.com/watch?v=PjzI4qvEWDE")
+	webbrowser.open("https://www.youtube.com/watch?v=cBVGlBWQzuc")
+	webbrowser.open("https://www.youtube.com/watch?v=3AtDnEC4zak")
+	webbrowser.open("https://www.youtube.com/watch?v=euCqAq6BRa4")
+
